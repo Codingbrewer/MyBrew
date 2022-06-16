198c322d4fe12efd65d5715259ee3a538d640bb9
Jordan <93078219+Codingbrewer@users.noreply.github.com>
2022-06-16 04:58:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './pages/Home';
-import Story from './pages/Story';
-import Recipes from './pages/Recipes';
-import Findme from './pages/Findme';
 import NavbarTop from './Components/NavbarTop';
 import Footer from './Components/Footer';
 function App() {
