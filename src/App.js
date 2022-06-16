3c3140a74aa527c61419085bc4e983d9adf65add
Jordan <93078219+Codingbrewer@users.noreply.github.com>
2022-06-16 05:25:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarTop from './Components/NavbarTop';
 import Footer from './Components/Footer';
+import Home from './pages/Home'
 function App() {
   return (
     
@@ -9,6 +10,9 @@ function App() {
 
 <NavbarTop />
 
+<Home />
+    
+    
 <footer>
 
   <Footer/>
