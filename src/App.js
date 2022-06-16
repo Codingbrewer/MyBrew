c6ffa1fae73f49a50d8141913a3cad422a6348ed
Jordan <93078219+Codingbrewer@users.noreply.github.com>
2022-06-16 05:46:33 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarTop from './Components/NavbarTop';
 import Footer from './Components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'
+
 function App() {
   return (
     
@@ -9,6 +12,16 @@ function App() {
 
 <NavbarTop />
 
+    <BrowserRouter>
+  <Routes>
+    <Route
+        path="*"
+        element={<Home />}
+    />
+  </Routes>
+</BrowserRouter>
+
+    
 <footer>
 
   <Footer/>
